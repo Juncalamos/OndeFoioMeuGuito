4517b937333d39b2a1aca75dd1779fd154d6b43d
Juncalamos <diogojuncal2@gmail.com>
2025-06-19 12:33:55 +0100

    Changed base in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // relative paths for manual deploy or subfolders
   plugins: [react()],
 })
